1ec56d19b44b61e4e25ae81d856a8377254aeecf
akin-aroge <aroogzbaba1@gmail.com>
2019-01-17 23:30:38 +0200

    added pelican-alchemy theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -12,6 +12,8 @@ TIMEZONE = 'Africa/Johannesburg'
 
 DEFAULT_LANG = 'en'
 
+THEME = './themes/pelican-alchemy/alchemy'
+
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
